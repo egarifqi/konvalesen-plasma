a3e0ee6a1830bca12d955496f0af7a5e0054313f
egarifqi <egarifqi@gmail.com>
2021-07-10 21:56:00 +0700

    init GA, new app.js file

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
+import "assets/scss/material-kit-react.scss?v=1.10.0";
+
+// pages for this product
+import LandingPage from "views/LandingPage/LandingPage.js";
+import ReactGa from "react-ga";
+
+var hist = createBrowserHistory();
+
+const App = () => {
+  useEffect(() => {
+    ReactGa.initialize("UA-195242118-1");
+    ReactGa.pageview(window.location.pathname + window.location.search);
+  }, []);
+
+  return (
+    <Router history={hist}>
+      <Switch>
+        <Route path="/" component={LandingPage} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
